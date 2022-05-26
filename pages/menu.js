5e57233cc6641312903e1de6026d989e492e7cd4
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-05-26 22:40:26 +0200

    test if i can listen to the menus

--- pages/menu.js
+++ pages/menu.js
@@ -1,4 +1,26 @@
+//React imports
+import { useState, useEffect } from "react";
+//Firebase imports
+import { db } from "@/firebase/firebase";
+import { onSnapshot, collection, query, orderBy } from "firebase/firestore";
+
 const Menu = () => {
+  const [data, setData] = useState([]);
+
+  // Subscribe to menus on firestore
+  useEffect(() => {
+    const q = query(collection(db, "menu"), orderBy("createdAt", "asc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const menus = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setData(menus);
+      console.log(menus);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return <div>menu</div>;
 };
 
