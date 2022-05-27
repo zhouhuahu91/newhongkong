ec3fd6c064f0b6b101260c347466210674bc70c6
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-05-27 20:45:38 +0200

    downloaded firebase

--- pages/menu.js
+++ pages/menu.js
@@ -3,9 +3,13 @@ import { useState, useEffect } from "react";
 
 // Hook imports
 import { useMenu } from "@/hooks/useMenu";
+import { useAuth } from "@/hooks/useAuth";
 
 const Menu = () => {
   const { data } = useMenu();
+  const { user } = useAuth();
+
+  console.log(user);
 
   return <div>menu</div>;
 };
