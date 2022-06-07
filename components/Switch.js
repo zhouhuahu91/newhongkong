b3643c72537ce83ac3f2ae27a6b195e938712f29
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-06-07 21:31:55 +0200

    fixed switch without use of framer motion

--- components/Switch.js
+++ components/Switch.js
@@ -1,26 +1,15 @@
-import { motion } from "framer-motion";
-
 const Switch = ({ toggle, onClick }) => {
   return (
     <button
       onClick={onClick}
-      // 18px doesn't exist in tailwind it goes from 16px to 20px.
-      // 30px also doesn't exist in tailwind it goes from 28px to 32px.
-      style={{ height: "18px", width: "30px" }}
-      className={`border rounded-full transition-colors duration-200 flex items-center ${
-        toggle ? "bg-main" : "bg-white"
-      }`}
+      className={`${
+        toggle && "bg-main"
+      } relative inline-flex flex-shrink-0 h-[18px] w-[30px] border rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-main focus-visible:ring-opacity-75`}
     >
-      <motion.div
-        animate={{
-          x: toggle ? "12px" : "0px",
-          transition: {
-            type: "tween",
-            duration: 0.14,
-          },
-        }}
-        className="w-4 h-4 transition-all rounded-full shadow bg-white"
-      ></motion.div>
+      <span
+        className={`${toggle ? "translate-x-[12px]" : "translate-x-0"}
+            pointer-events-none inline-block h-[16px] w-[16px] rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
+      />
     </button>
   );
 };
