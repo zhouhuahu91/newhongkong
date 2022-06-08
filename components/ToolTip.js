5e8ca5b33e3ae72da58eb2f0a07f1b6205579c7b
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-06-08 09:11:45 +0200

    finished tooltip with help of floating-ui

--- components/ToolTip.js
+++ components/ToolTip.js
@@ -1,43 +1,50 @@
 // React imports
 import { useState } from "react";
 // Third party imports
+import { useFloating } from "@floating-ui/react-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
-// Tooltip that shows a question mark with a short explanation when hovered.
-const Tooltip = ({ children, tip, disabled }) => {
-  const [show, setShow] = useState(false);
+const Tooltip = ({ tip, disabled }) => {
+  const [open, setOpen] = useState(false);
+  // We need this third party library to make the tooltip show outside of the scroll area.
+  // Position relative doesn't work when the tooltip is outside of the scroll area.
+  const { x, y, reference, floating, strategy } = useFloating({
+    open,
+    placement: "top",
+  });
+
   return (
-    <div className="relative">
+    <>
+      <span
+        ref={reference}
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+        className={`material-symbols-rounded text-lg ${
+          disabled ? "text-gray-300" : ""
+        }`}
+      >
+        help
+      </span>
       <AnimatePresence>
-        {show && (
+        {open && (
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="absolute left-1/2 bottom-7 text-xs text-white shadow-sm bg-main p-2 rounded -translate-x-1/2 w-56"
+            initial={{ opacity: 0, scale: 0.85 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring", damping: 20, stiffness: 300 }}
+            exit={{ opacity: 0, scale: 0.85 }}
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
+            }}
+            className="border p-2 rounded bg-main text-white text-sm w-52"
           >
             {tip}
           </motion.div>
         )}
       </AnimatePresence>
-      <div
-        onMouseOver={() => setShow(true)}
-        onMouseLeave={() => setShow(false)}
-        className="flex"
-      >
-        {children ? (
-          children
-        ) : (
-          <span
-            className={`material-symbols-rounded text-lg ${
-              disabled ? "text-gray-300" : ""
-            }`}
-          >
-            help
-          </span>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
--- components/cart/Cart.js
+++ components/cart/Cart.js
@@ -1,3 +1,5 @@
+// React imports
+import { useRef, useEffect } from "react";
 // Hook imports
 import useI18n from "@/hooks/useI18n";
 import { useCart } from "@/hooks/useCart";
@@ -13,109 +15,154 @@ const Cart = () => {
   const t = useI18n();
   // Return the cart.
   const { cartState, dispatch } = useCart();
+  // We need the ref to scroll to the last item added to cart when cart is longer than the screen.
+  const ref = useRef();
+  useEffect(() => {
+    // TODO: smooth scroll not working on ios.
+    // We get the scroll distance by subtracting the clientHeight form the scrollheight.
+    const scroll = ref.current.scrollHeight - ref.current.clientHeight;
+    // We scrollTo the it with a behavior of smooth.
+    // Not 100% sure how this works but it works!
+    ref.current.scrollTo({ top: scroll, behavior: "smooth" });
+  }, [cartState.cart]); // Triggers everytime the cart updates.
 
   return (
-    <div style={{ maxHeight: "calc(100vh - 185px)" }} className="select-none">
-      {cartState.cart.map((cartItem) => {
-        return (
-          // This is the main container where the cart items are.
-          <div
-            key={cartItem.id}
-            className="flex mt-2 items-start overflow-auto px-4"
-          >
-            {/* This container div is where we can increment and decrement the item in the cart. */}
-            <div
-              style={{ maxWidth: "3.25rem" }}
-              className="flex justify-between w-full items-center"
-            >
-              <IconButton
-                variant="remove_circle_outline"
-                size="small"
-                color="main"
-                onClick={() =>
-                  dispatch({
-                    type: "DECREMENT_ITEM",
-                    payload: cartItem,
-                  })
-                }
-              />
-              <span>{cartItem.qwt}</span>
-              <IconButton
-                variant="add_circle_outline"
-                size="small"
-                color="main"
-                onClick={() =>
-                  dispatch({
-                    type: "INCREMENT_ITEM",
-                    payload: cartItem,
-                  })
-                }
-              />
-            </div>
-            {/* This is the cointainer for the cart item name and description. */}
-            <div className="flex-grow mx-3">
-              <div>
-                <span>{cartItem.name[t.locale]}</span>
-              </div>
-              <div className="leading-none">
-                {/* See modal for how the item description gets generated. */}
-                <span className="text-xs text-gray-500">
-                  {cartItem.description[t.locale]}
-                  {/* {getItemDescription(item, t.locale)} */}
-                </span>
-              </div>
-            </div>
-            {/* This is the cointainer for the cart item price. */}
-            <div className="justify-self-end">
-              <span>{euro(cartItem.price)}</span>
-            </div>
-          </div>
-        );
-      })}
-      {/* This is the container for subtotal, tips, delivery cost and total.  */}
-      <div className="p-4 mt-6">
-        <div className="border-t pt-4">
-          <div className="flex justify-between mt-2">
-            <span>{t.subtotal}</span>
-            <span>{euro(cartState.cart.reduce((x, y) => x + y.price, 0))}</span>
-          </div>
-          {/* If they selecte delivery we show the delivery price. */}
-          {cartState.delivery && cartState.delivery !== "undecided" && (
-            <div className="flex justify-between mt-2">
-              <span>{t.delivery_fee}</span>
-              <span>{euro(250)}</span>
-            </div>
-          )}
-          {/* If they do NOT pay with cash we add the cost for transaction. */}
-          {cartState.paymentMethod !== "cash" &&
-            cartState.paymentMethod !== "undecided" && (
-              <div className="flex justify-between mt-2">
-                <span>{t.transaction_fee}</span>
-                <span>{euro(30)}</span>
-              </div>
-            )}
-          {/* If they select for pick up they have to pay for the platic bag or... */}
-          {/* they have to bring their own. */}
-          {(!cartState.delivery ||
-            cartState.delivery === "undecided" ||
-            true) && (
-            <div className="flex justify-between mt-2">
-              <div className="flex space-x-1 items-center">
-                <span>{t.bag}</span>
-                <Tooltip tip={t.bag_tooltip} />
-              </div>
-              <div className="flex items-center space-x-4">
-                <Switch
-                  toggle={cartState.bag}
-                  onClick={() => dispatch({ type: "TOGGLE_BAG" })}
+    //  ********* MAIN CART CONTAINER *********
+    <>
+      {/* ********* CART ITEMS CONTAINER ********* */}
+      <div
+        ref={ref}
+        style={{ maxHeight: "calc(100vh - 170px)" }}
+        className="select-none overflow-auto flex-grow px-4 pb-4"
+      >
+        {cartState.cart.map((cartItem) => {
+          return (
+            // ********* CART ITEM CONTAINER *********
+            <div key={cartItem.id} className="flex items-start mt-1">
+              {/* ********** INCREMENT & DECEREMENT ********** */}
+              <div
+                style={{ maxWidth: "3.25rem" }}
+                className="flex justify-between w-full items-center"
+              >
+                <IconButton
+                  variant="remove_circle_outline"
+                  size="small"
+                  color="main"
+                  onClick={() =>
+                    dispatch({
+                      type: "DECREMENT_ITEM",
+                      payload: cartItem,
+                    })
+                  }
                 />
-                <span>{cartState.bag ? euro(10) : euro(0)}</span>
+                <span>{cartItem.qwt}</span>
+                <IconButton
+                  variant="add_circle_outline"
+                  size="small"
+                  color="main"
+                  onClick={() =>
+                    dispatch({
+                      type: "INCREMENT_ITEM",
+                      payload: cartItem,
+                    })
+                  }
+                />
               </div>
+              {/* ********** END INCREMENT & DECEREMENT ********** */}
+              {/* ********** PRODUCT NAME & DESCRIPTION ********** */}
+              <div className="flex-grow mx-3">
+                <div>
+                  <span>{cartItem.name[t.locale]}</span>
+                </div>
+                <div className="leading-none">
+                  <span className="text-xs text-gray-500">
+                    {cartItem.description[t.locale]}
+                  </span>
+                </div>
+              </div>
+              {/* ********** END PRODUCT NAME & DESCRIPTION ********** */}
+              {/* ********** PRICE ********** */}
+              <div className="justify-self-end">
+                <span>{euro(cartItem.price)}</span>
+              </div>
+              {/* ********** PRICE ********** */}
+            </div>
+            // ********* END CART ITEM CONTAINER *********
+          );
+        })}
+        {/* ******** SUBTOTAL ********* */}
+        <div className="flex justify-between border-t mt-6 pt-4">
+          <span>{t.subtotal}</span>
+          <span>{euro(cartState.cart.reduce((x, y) => x + y.price, 0))}</span>
+        </div>
+        {/* ********* END SUBTOTAL ********** */}
+        {/* ******** DELIVERY FEE ********* */}
+        {cartState.delivery && cartState.delivery !== "undecided" && (
+          <div className="flex justify-between mt-1">
+            <span>{t.delivery_fee}</span>
+            <span>{euro(250)}</span>
+          </div>
+        )}
+        {/* ******** END DELIVERY FEE ********* */}
+        {/* ******** DELIVERY FEE ********* */}
+        {cartState.paymentMethod !== "cash" &&
+          cartState.paymentMethod !== "undecided" && (
+            <div className="flex justify-between mt-1">
+              <span>{t.transaction_fee}</span>
+              <span>{euro(30)}</span>
             </div>
           )}
+        {/* ******** END DELIVERY FEE ********* */}
+        {/* ******** BAG FEE ********* */}
+        {/* If the user selects for pick up they have to pay for the platic bag or... */}
+        {/* they have to bring their own. */}
+        {(!cartState.delivery ||
+          cartState.delivery === "undecided" ||
+          true) && (
+          <div className="flex justify-between mt-1">
+            <div className="flex space-x-1 items-center">
+              <span>{t.bag}</span>
+              <Tooltip tip={t.bag_tooltip} />
+            </div>
+            <div className="flex items-center space-x-4">
+              <Switch
+                toggle={cartState.bag}
+                onClick={() => dispatch({ type: "TOGGLE_BAG" })}
+              />
+              <span>{cartState.bag ? euro(10) : euro(0)}</span>
+            </div>
+          </div>
+        )}
+        {/* ******** END BAG FEE ********* */}
+      </div>
+      {/* ********* CART SUMMARY CONTAINER ********* */}
+      <div className="mx-4 pb-4 border-t">
+        {/* ********* TIP CONTAINER ********* */}
+        <div className="flex justify-between pt-4 mt-4">
+          <span>{t.restaurant_tip}</span>
+          <div className="flex items-center space-x-2">
+            <IconButton
+              size="small"
+              color="main"
+              variant="remove_circle_outline"
+              onClick={() => dispatch({ type: "DECREMENT_TIP" })}
+            />
+            <IconButton
+              size="small"
+              color="main"
+              variant="add_circle_outline"
+              onClick={() => dispatch({ type: "INCREMENT_TIP" })}
+            />
+            <span>{euro(cartState.tip)}</span>
+          </div>
+        </div>
+        <div className="flex justify-between mt-1">
+          <span>{t.total}</span>
+          <span>{euro(1000)}</span>
         </div>
       </div>
-    </div>
+    </>
+    // ********* END MAIN CART CONTAINER *********
   );
 };
 
