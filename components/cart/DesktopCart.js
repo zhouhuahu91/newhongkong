34264b66976c1dddfd5f93cc69f4ba59724250ff
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-06-15 17:35:47 +0200

    you can only change from delivery and pick up at menu

--- components/cart/DesktopCart.js
+++ components/cart/DesktopCart.js
@@ -55,7 +55,7 @@ const DesktopCart = ({ setOpen, setDelivery }) => {
           </div>
         )}
       </div>
-      {cartState.delivery !== "undecided" && (
+      {cartState.delivery !== "undecided" && atMenu && (
         <div className="mx-auto max-w-sm w-full px-2">
           <button
             onClick={() => {
