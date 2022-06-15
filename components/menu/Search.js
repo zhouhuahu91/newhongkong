61a476520f12a7f551e9cc249e191837cd598ee5
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-06-15 14:55:39 +0200

    finished search input animation

--- components/menu/Search.js
+++ components/menu/Search.js
@@ -1,18 +1,41 @@
 // React imports
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useState, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import useOnClickOutside from "@/hooks/useOnClickOutside";
+// Hook imports
 
 const Search = () => {
   const [open, setOpen] = useState(false);
+  const el = useRef();
+
+  useOnClickOutside(el, () => setOpen(false));
+
   return (
-    <>
-      {open && (
-        <motion.div
-          initial={{ scaleX: 0 }}
-          animate={{ scaleX: 1 }}
-          className="absolute bg-white z-10 border h-11 w-full max-w-sm shadow rounded-full"
-        ></motion.div>
-      )}
+    <div ref={el}>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            sizeTransition
+            initial={{ width: 0 }}
+            animate={{
+              width: 384,
+              transition: {
+                duration: 0.5,
+                type: "spring",
+                stiffness: 300,
+                damping: 15,
+              },
+            }}
+            exit={{ width: 44, transition: { duration: 0.2 } }}
+            className="absolute bg-white z-10 border h-11 max-w-sm shadow rounded-full flex items-center justify-center"
+          >
+            <input
+              autoFocus
+              className="w-full mx-14 h-full appearance-none focus:outline-none text-sm"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <button
         onClick={() => setOpen((prev) => !prev)}
         type="button"
@@ -22,7 +45,7 @@ const Search = () => {
       >
         <span className="material-symbols-rounded">search</span>
       </button>
-    </>
+    </div>
   );
 };
 
