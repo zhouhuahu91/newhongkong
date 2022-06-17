45009259030c5f0e3ab4b31540bdac6173c05494
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-06-17 20:03:21 +0200

    added pick up or delivery button

---
+++ components/checkout/PickUpOrDelivery.js
@@ -0,0 +1,48 @@
+// Hook imports
+import { useCart } from "@/hooks/useCart";
+import useI18n from "@/hooks/useI18n";
+
+const PickUpOrDelivery = () => {
+  const {
+    dispatch,
+    cartState: { delivery },
+  } = useCart();
+  const t = useI18n();
+
+  // Style of the button.
+  const btnStyle =
+    "flex items-center justify-center text-sm py-2 focus:outline-none font-medium w-1/2 border transition-colors duration-200 ease-in-out";
+
+  return (
+    <>
+      <h2 className="text-lg my-2">{t.pickup_delivery}</h2>
+
+      <div className="flex justify-between">
+        {/* This button sets delivery to false which means the customer will pcik up the order */}
+        <button
+          onClick={() => dispatch({ type: "SET_DELIVERY", payload: false })}
+          type="button"
+          className={`${btnStyle} rounded-l-md ${
+            delivery === false ? "bg-neutral-100" : "shadow-md bg-white"
+          }`}
+        >
+          <span className="material-symbols-rounded mr-3">store</span>
+          {t.pick_up}
+        </button>
+        {/* This button sets delivery to true which means the order will be delivered. */}
+        <button
+          onClick={() => dispatch({ type: "SET_DELIVERY", payload: true })}
+          type="button"
+          className={`${btnStyle} rounded-r-md ${
+            delivery === true ? "bg-neutral-100" : "shadow-md bg-white"
+          }`}
+        >
+          <span className="material-symbols-rounded mr-3">pedal_bike</span>
+          {t.delivery}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default PickUpOrDelivery;
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,11 +1,35 @@
+// Hook imports
+import useI18n from "@/hooks/useI18n";
+// Component imports
 import DesktopCart from "@/components/cart/DesktopCart";
 import MobileCart from "@/components/cart/MobileCart";
+import PickUpOrDelivery from "@/components/checkout/PickUpOrDelivery";
+// Form imports
+import { useForm, useFormState } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const CheckOut = () => {
+  // t is used to translate text.
+  const t = useI18n();
+
   return (
-    <div className="relative">
+    <div className="w-full max-w-screen-xl mx-auto relative">
       <MobileCart />
-      <DesktopCart />
+      <div className="grid md:grid-cols-12 gap-6 mx-4">
+        {/* Container for left part of the content, the form. */}
+        <div className="col-span-12 md:col-span-6 lg:col-span-7 mb-20 w-full">
+          {/* Main title of the checkout form. */}
+          <h1 className="text-5xl uppercase font-semibold mt-4 mb-8">
+            {t.almost_done}
+          </h1>
+          <PickUpOrDelivery />
+        </div>
+        {/* Container for the cart. */}
+        <div className="col-span-6 lg:col-span-5">
+          <DesktopCart />
+        </div>
+      </div>
     </div>
   );
 };
