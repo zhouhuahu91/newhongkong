20fc6ef43d065467e1e79191162ebed80b43ee8e
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-06-23 19:25:22 +0200

    needs to parse the key to use it on vercel

--- firebase/admin.js
+++ firebase/admin.js
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       project_id: process.env.NEXT_PUBLIC_PROJECTID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
