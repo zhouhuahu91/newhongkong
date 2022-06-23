3c29ba3e52da88f88129b61425cf413d42c2512f
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-06-23 21:26:37 +0200

    we check if there is a private key first before replacing it.

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,11 +1,18 @@
 // firebase admin can only be used on the server.
 const admin = require("firebase-admin");
 
+let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+// If there is a private key we replace remove the blanks.
+// We do this because vercel does not allow blanks in the private key.
+if (privateKey) {
+  privateKey = privateKey.replace(/\\n/g, "\n");
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       project_id: process.env.NEXT_PUBLIC_PROJECTID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      private_key: privateKey,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
