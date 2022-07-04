5fa295c7e48b326c34d17ebe373c3dabdd287ad5
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-07-04 13:21:27 +0200

    added a datepicker

---
+++ components/DatePicker.js
@@ -0,0 +1,54 @@
+// React imports
+import { useState, useRef } from "react";
+// Hook imports
+import useOnClickOutside from "@/hooks/useOnClickOutside";
+// Function imports
+import getCurrentDate from "@/functions/getCurrentDate";
+// Date Picker imports
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const DatePickerComponent = ({ setDate, className, date }) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  // This is a reference to the div surrounding this component.
+  const ref = useRef();
+  useOnClickOutside(ref, () => setShow(false));
+
+  return (
+    <div
+      ref={ref}
+      className={`relative ${className && className} flex select-none`}
+    >
+      <button
+        onClick={() => {
+          setShow((prev) => !prev);
+        }}
+        type="button"
+        className="flex items-center"
+      >
+        <span className={`font-bold mt-1 mr-2`}>{date}</span>
+        <span
+          className={`material-symbols-rounded cursor-pointer ${
+            getCurrentDate() !== date ? "text-main" : "text-gray-700"
+          }`}
+        >
+          today
+        </span>
+      </button>
+      <div className={`absolute top-10 right-0 ${!show && "hidden"} z-10`}>
+        <DatePicker
+          selected={selectedDate}
+          onChange={(d) => {
+            setShow(false);
+            setSelectedDate(d);
+            setDate(getCurrentDate(d));
+          }}
+          inline
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DatePickerComponent;
--- components/dashboard/DashboardHeader.js
+++ components/dashboard/DashboardHeader.js
@@ -1,5 +1,35 @@
+// NextJs imports
+import Link from "next/link";
+// Hook imports
+import { useStoreInfo } from "@/hooks/useStoreInfo";
+// React imports
+import { useState } from "react";
+// Component imports
+import DatePicker from "@/components/DatePicker";
+// Function imports
+import getCurrentDate from "@/functions/getCurrentDate";
+
 const DashboardHeader = () => {
-  return <header>header</header>;
+  const [date, setDate] = useState(getCurrentDate());
+  const { digitalCurrentTime } = useStoreInfo();
+
+  return (
+    <header className="bg-white shadow-sm border-b">
+      <div className="mx-auto max-w-screen-lg flex justify-between h-16 items-center px-4">
+        <Link href="/">
+          <a
+            className={`text-2xl font-bold red-focus-ring rounded-md px-1 text-main`}
+          >
+            nHK
+          </a>
+        </Link>
+        <div className="flex items-center space-x-3">
+          <span className="font-semibold mt-1">{digitalCurrentTime}</span>
+          <DatePicker date={date} setDate={setDate} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default DashboardHeader;
