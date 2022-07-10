b78437f4ca8880bbc810e205cb199aef7ac7ebcc
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-07-10 22:27:12 +0200

    made a dashboard card

---
+++ components/dashboard/DashboardCard.js
@@ -0,0 +1,226 @@
+// React imports
+import { useState, useEffect } from "react";
+// NextJS imports
+import Image from "next/image";
+// Animation imports
+import { motion } from "framer-motion";
+// Firebase imports
+import { db } from "@/firebase/firebase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+// Hook imports
+import { useAuth } from "@/hooks/useAuth";
+// Function imports
+import euro from "@/functions/euro";
+
+const DashboardCard = ({ order }) => {
+  const [open, setOpen] = useState(false);
+  const { user } = useAuth();
+
+  const googleDirectionsLink = `https://www.google.com/maps/dir/?api=1&destination=${
+    order.address.city
+  }+${order.address.street}+${order.address.houseNumber}${
+    order.address.addition ? `+${order.address.addition}` : ""
+  }&travelmode=bicycling`;
+
+  // listen for enter key
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && open) {
+      // if enter key pressed we set order to printed
+      // we get the ref
+      const ref = doc(db, `orders/${order.id}`);
+      updateDoc(ref, {
+        printed: true,
+      });
+      setOpen(false);
+    }
+    // if escape key is pressed and modal is open we close the modal
+    if (e.key === "Escape" && open) {
+      setOpen(false);
+    }
+  };
+  // listen for enter key with useEffect
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [open]);
+
+  return (
+    <motion.div
+      whileHover={{ scale: 1.03 }}
+      layout
+      initial={{ x: -100, opacity: 0 }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.2, type: "spring", delay: 0.2 },
+      }}
+      className={`border bg-white hover:shadow-lg rounded-xl p-4 col-span-12 sm:col-span-6 xl:col-span-4 space-y-1 ${
+        !order.paymentMethod === "online" && !order.paid
+          ? "cursor-wait"
+          : "cursor-pointer"
+      }`}
+      onClick={() => {
+        setOpen(true);
+      }}
+    >
+      <div className="flex items-center space-x-3 justify-between">
+        <div className="flex items-center space-x-3">
+          <h3 className={`font-semibold text-2xl`}>{order.name}</h3>
+          {/* We can delete orders if the order is placed by an admin or... */}
+          {/* ... order is placed but but payment process is never finished */}
+          {(order.email.toLowerCase() === "test@test.com" ||
+            (Date.now() > order.createdAt + 300000 &&
+              !order.paymentMethod === "online" &&
+              !order.paid)) &&
+            user?.admin && (
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  const ref = doc(db, `orders/${order.id}`);
+                  deleteDoc(ref);
+                }}
+                className="material-symbols-rounded text-gray-100 hover:text-main"
+              >
+                delete
+              </span>
+            )}
+        </div>
+        {/* If order is not printed we show the print icon */}
+        {!order.printed && (
+          <span
+            onClick={(e) => {
+              e.stopPropagation();
+              const ref = doc(db, `orders/${order.id}`);
+              updateDoc(ref, {
+                printed: true,
+              });
+            }}
+            className="material-symbols-rounded"
+          >
+            print
+          </span>
+        )}
+        {/* If order is rinted but not yet ready we show the shopping bag icon indicating... */}
+        {/* the order is ready and packed. */}
+        {order.printed && !order.ready && (
+          <span
+            onClick={(e) => {
+              e.stopPropagation();
+              const ref = doc(db, `orders/${order.id}`);
+              updateDoc(ref, {
+                ready: true,
+              });
+            }}
+            className={`material-symbols-rounded`}
+          >
+            inventory
+          </span>
+        )}
+        {/* If the order is ready and paid for but not yet completed we show... */}
+        {/* the icon that indicates that it can be put on completed. */}
+        {order.ready && order.paid && !order.completed && (
+          <span
+            onClick={(e) => {
+              e.stopPropagation();
+              const ref = doc(db, `orders/${order.id}`);
+              updateDoc(ref, {
+                completed: true,
+              });
+            }}
+            className={`material-symbols-rounded`}
+          >
+            task_alt
+          </span>
+        )}
+        {/* If the order is completed we show the icon that can remove the order from being completed. */}
+        {order.completed && (
+          <span
+            onClick={(e) => {
+              e.stopPropagation();
+              const ref = doc(db, `orders/${order.id}`);
+              updateDoc(ref, {
+                completed: false,
+              });
+            }}
+            className={`material-symbols-rounded`}
+          >
+            undo
+          </span>
+        )}
+      </div>
+      <div className="flex justify-between items-center">
+        <div>
+          <div className="flex items-center space-x-2">
+            <span className="text-xl font-semibold">
+              {order.time.includes(":") ? order.time : "asap"}
+            </span>
+            {order.delivery && (
+              <a
+                href={googleDirectionsLink}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => e.stopPropagation()}
+                className="flex"
+              >
+                <span className="material-symbols-rounded">pedal_bike</span>
+              </a>
+            )}
+          </div>
+          <span className="text-sm">
+            {order.delivery &&
+              `${order.address.street} ${order.address.houseNumber}${
+                order.address.addition ? `-${order.address.addition}` : ""
+              }`}
+          </span>
+        </div>
+        <div className="flex items-center">
+          <span className="text-xl font-semibold mr-3">
+            {euro(order.total)}
+          </span>
+          {/* If user selectes Ideal and the order is not paid yet we show a spinner. */}
+          {order.paymentMethod === "online" && !order.paid ? (
+            <div
+              className={`rounded-full border-white border-t-main border-2 animate-spin w-5 h-5`}
+            />
+          ) : order.paymentMethod === "in_person" ? (
+            // If it is paid we show a green credit card otherwise we show a red credit card.
+            <span
+              onClick={(e) => {
+                if (order.paymentMethod === "online") return;
+                e.stopPropagation();
+                const ref = doc(db, `orders/${order.id}`);
+                updateDoc(ref, {
+                  paid: !order.paid,
+                });
+              }}
+              className={`material-symbols-rounded ${
+                order.paid ? "text-green-600" : "text-main"
+              }`}
+            >
+              {order.paid ? "credit_score" : "credit_card_off"}
+            </span>
+          ) : (
+            <div className="flex items-center">
+              <Image
+                src="/paymentIcons/ideal.svg"
+                alt="ideal icon"
+                width={24}
+                height={24}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      {order.remarks && (
+        <div className="mt-6 flex flex-col">
+          <span className="text-sm text-gray-500">Remarks:</span>
+          <span className="text-sm font-normal">{order.remarks}</span>
+        </div>
+      )}
+    </motion.div>
+  );
+};
+
+export default DashboardCard;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { useStoreInfo } from "@/hooks/useStoreInfo";
 // Component imports
 import Header from "@/components/dashboard/DashboardHeader";
+import DashboardCard from "@/components/dashboard/DashboardCard";
 // Firebase imports
 import { db } from "@/firebase/firebase";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
@@ -24,6 +25,62 @@ const Dashboard = () => {
   // 4. Orders that are being delivered.
   const [delivery, setDelivery] = useState([]);
 
+  useEffect(() => {
+    const q = query(collection(db, "orders"), where("date", "==", date));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const data = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      // We sort the data by time.
+      data.sort((a, b) => {
+        // If a.time or b.time includes ":" it means...
+        // the order isn't asap and we set x or y to 0.
+        // This way that order is always on top.
+        let x = a.time.includes(":")
+          ? a.time.slice(0, 5).replace(":", "")
+          : "0";
+        let y = b.time.includes(":")
+          ? b.time.slice(0, 5).replace(":", "")
+          : "0";
+        return x - y;
+      });
+
+      const filtered = data.filter((order) =>
+        showCompleted ? order : order.completed === false
+      );
+
+      // Sets newOrders to orders that haven't been printed and aren't ready yet.
+      setNewOrders(
+        filtered.filter((order) => {
+          if (!order.printed) return order;
+        })
+      );
+
+      // Sets printed to orders that are printed and that aren't ready yet.
+      setPrinted(
+        filtered.filter((order) => {
+          if (order.printed && !order.ready) return order;
+        })
+      );
+
+      // Sets delivery to orders that are for delivery and are ready.
+      setDelivery(
+        filtered.filter((order) => {
+          if (order.delivery && order.ready) return order;
+        })
+      );
+
+      // Sets pickup to orders that are for pickup and are ready.ƒ
+      setPickup(
+        filtered.filter((order) => {
+          if (!order.delivery && order.ready) return order;
+        })
+      );
+    });
+
+    return () => unsubscribe();
+  }, [date, showCompleted]);
+
   return (
     <div>
       <Header
@@ -32,6 +89,50 @@ const Dashboard = () => {
         showCompleted={showCompleted}
         setShowCompleted={setShowCompleted}
       />
+      <div className="p-4 select-none">
+        <div className="grid gap-4 grid-cols-12">
+          <div className="col-span-12 md:col-span-6 xl:col-span-3 flex flex-col px-4">
+            <h1 className="text-2xl mb-4 font-semibold text-center border-b">
+              NEW
+            </h1>
+            <div className="grid gap-4">
+              {newOrders.map((order) => (
+                <DashboardCard key={order.id} order={order} />
+              ))}
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6 xl:col-span-3 flex flex-col px-4">
+            <h1 className="text-2xl mb-4 font-semibold text-center border-b">
+              KITCHEN
+            </h1>
+            <div className="grid gap-4">
+              {printed.map((order) => (
+                <DashboardCard key={order.id} order={order} />
+              ))}
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6 xl:col-span-3 flex flex-col px-4">
+            <h1 className="text-2xl mb-4 font-semibold text-center border-b">
+              PICK UP
+            </h1>
+            <div className="grid gap-4">
+              {pickup.map((order) => (
+                <DashboardCard key={order.id} order={order} />
+              ))}
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6 xl:col-span-3 flex flex-col px-4">
+            <h1 className="text-2xl mb-4 font-semibold text-center border-b">
+              DELIVERY
+            </h1>
+            <div className="grid gap-4">
+              {delivery.map((order) => (
+                <DashboardCard key={order.id} order={order} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
