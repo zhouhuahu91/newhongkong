51847e9a3c04cfd75fef188daf45784639795806
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-07-23 21:23:56 +0200

    enable-background needs to be enableBackground

--- components/icons/ManageAccountsIcon.js
+++ components/icons/ManageAccountsIcon.js
@@ -2,7 +2,7 @@ const ManageAccountsIcon = ({ color, width, height }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 24 24"
+      enableBackground="new 0 0 24 24"
       height={height ? height : "24"}
       viewBox="0 0 24 24"
       width={width ? width : "24"}
