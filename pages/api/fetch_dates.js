4a8497c449c427764384518762ee481c5f5ff1b0
Zhouhua Hu <zhouhua.hu@gmail.com>
2022-08-01 07:48:29 +0200

    made a new api so that sold site can still fetch it

---
+++ pages/api/fetch_dates.js
@@ -0,0 +1,26 @@
+import Cors from "micro-cors";
+import getCurrentDate from "@/functions/getCurrentDate";
+
+// To allow cross origin.
+const cors = Cors({
+  allowMethods: ["POST", "HEAD"],
+});
+
+const fetchDates = (req, res) => {
+  try {
+    const time_zone = new Date().toLocaleString("en-Us", {
+      timeZone: "Europe/Amsterdam",
+    });
+    const date = new Date(time_zone);
+    const day = date.getDay();
+    const currentDate = getCurrentDate(date);
+    return res.status(200).json({
+      dates: { currentDate, day },
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ e });
+  }
+};
+
+export default cors(fetchDates);
