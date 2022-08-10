74dd9802d336180b63973d06ac217cb21aba228b
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-08-10 19:17:41 +0200

    added tsconfig file and added converted first file to ts

--- functions/getURL.ts
+++ functions/getURL.ts
@@ -1,8 +1,10 @@
 // This function return URL depending if we are in production or development.
 // If we are in production we return the URL of the server.
 // If we are in development we return the URL of the localhost.
-const getURL = () => {
-  let URL = "https://newhongkong.nl";
+type getURLType = () => string;
+
+const getURL: getURLType = () => {
+  let URL: string = "https://newhongkong.nl";
   if (process.env.NODE_ENV === "development") URL = "http://localhost:3000";
 
   return URL;
