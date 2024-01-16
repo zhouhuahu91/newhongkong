3ebf141cc61ed2df59b0c97ccf3e9bb7b124f434
zhouhuahu91 <Zhouhua.hu@gmail.com>
2024-01-16 16:09:31 +0100

    removed poi from map

--- pages/succes.js
+++ pages/succes.js
@@ -101,7 +101,7 @@ const Succes = () => {
           {/* TODO: maybe return a map of direction to the user if it is delivery. */}
           <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLEMAPS_API}>
             <div className="w-auto h-[480px] overflow-hidden roundedb-xl">
-              <Map zoom={18} center={position}>
+              <Map zoom={18} center={position} mapId="939f6f3f30a43f1a">
                 <Marker position={position} />
               </Map>
             </div>
