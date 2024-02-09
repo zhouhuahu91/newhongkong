dec895e29ff460f721ffb1f79de35bb62c463884
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-02-09 12:08:12 +0100

    now shows complete date

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -28,7 +28,7 @@ const DatePickerComponent = ({ setDate, className, date }) => {
         type="button"
         className="flex items-center red-focus-ring px-1 rounded-md"
       >
-        <span className={`mt-1 mr-2 font-medium`}>{date.slice(0, 5)}</span>
+        <span className={`mt-1 mr-2 font-medium hidden sm:block`}>{date}</span>
         <CalendarIcon
           className={`${getCurrentDate() !== date && "fill-main"}`}
         />
---
+++ pages/tables.js
@@ -0,0 +1,9 @@
+const Tables = () => {
+  return (
+    <div className="h-full w-full flex justify-center items-center mt-20">
+      <div className="grid grid-cols-12 gap-4 w-full h-full max-w-screen-md"></div>
+    </div>
+  );
+};
+
+export default Tables;
