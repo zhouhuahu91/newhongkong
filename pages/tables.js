6d7aa694d618d8e7547f3fd48b61e81609b3c8c2
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-02-12 12:45:48 +0100

    added firebase

--- pages/tables.js
+++ pages/tables.js
@@ -1,5 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+// Firebase imports
+import { db } from "@/firebase/firebase";
+import {
+  collection,
+  addDoc,
+  query,
+  updateDoc,
+  doc,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 // Hook imports
 import { useStoreInfo } from "@/hooks/useStoreInfo";
 import { useAuth } from "@/hooks/useAuth";
@@ -11,7 +22,36 @@ import StoreLayout from "@/tables/StoreLayout";
 const Tables = () => {
   const { currentDate } = useStoreInfo();
   const { user } = useAuth();
+  const [tables, setTables] = useState([]);
   const [date, setDate] = useState(currentDate);
+
+  const createNewTable = () => {
+    const table = {
+      number: tables.length + 1,
+      position: {
+        x: 0,
+        y: 0,
+      },
+      paid: false,
+      paymentMethodType: null,
+      date: currentDate,
+      createdAt: Date.now(),
+    };
+
+    addDoc(collection(db, "tables"), table);
+  };
+
+  useEffect(() => {
+    const q = query(collection(db, "tables"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const data = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      setTables(data);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   // Tables page is only for admin...
   // If no user is not fetched yet we show spinner
@@ -33,12 +73,38 @@ const Tables = () => {
       <div className="w-full max-w-screen-xl mx-auto">
         <div className="w-full border rounded-xl relative h-[756px] mt-o xl:mt-20">
           <StoreLayout />
-          <motion.div
-            drag
-            className="w-20 aspect-square bg-white rounded-md border shadow-xl flex items-center justify-center text-3xl font-bold"
+          {tables.map((table, idx) => {
+            const ref = doc(db, `tables/${table.id}`);
+            return (
+              <motion.div
+                style={{
+                  top: table.position.y,
+                  left: table.position.x,
+                }}
+                key={table.id}
+                onMouseUp={(e) =>
+                  updateDoc(ref, {
+                    position: {
+                      x: e.screenX,
+                      y: e.screenY,
+                    },
+                  })
+                }
+                drag
+                dragSnapToOrigin
+                className={`fixed w-24 aspect-square bg-white rounded-md border shadow-xl flex items-center justify-center text-3xl font-bold`}
+              >
+                {table.number}
+              </motion.div>
+            );
+          })}
+          <button
+            onClick={() => createNewTable()}
+            type="button"
+            className="button absolute bottom-3 right-80 bg-main text-white w-40"
           >
-            6
-          </motion.div>
+            Nieuwe tafel
+          </button>
         </div>
       </div>
     </>
