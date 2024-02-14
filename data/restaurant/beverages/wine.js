4482100767801c497425ec028f23c6b5048e4000
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-02-14 21:45:36 +0100

    added wine and beer

---
+++ data/restaurant/beverages/wine.js
@@ -0,0 +1,43 @@
+import btw from "../../options/btw";
+
+export default {
+  name: "wine",
+  items: [
+    {
+      id: "1200",
+      name: "droge witte wijn",
+      price: 460,
+      btw: btw.hoog,
+    },
+    {
+      id: "1201",
+      name: "zoete witte wijn",
+      price: 460,
+      btw: btw.hoog,
+    },
+    {
+      id: "1202",
+      name: "rode wijn",
+      price: 460,
+      btw: btw.hoog,
+    },
+    {
+      id: "1203",
+      name: "rose",
+      price: 460,
+      btw: btw.hoog,
+    },
+    {
+      id: "1204",
+      name: "port",
+      price: 480,
+      btw: btw.hoog,
+    },
+    {
+      id: "1205",
+      name: "sherry",
+      price: 480,
+      btw: btw.hoog,
+    },
+  ],
+};
