ef8a71fe83ccaad9a294be3f6ade36701d344652
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-02-15 19:17:24 +0100

    added strong drinks

---
+++ data/restaurant/beverages/strongDrinks.js
@@ -0,0 +1,44 @@
+import btw from "../../options/btw";
+
+export default {
+  name: "sterke dranken",
+  id: "1500",
+  items: [
+    {
+      id: "1501",
+      name: "chivas regal",
+      price: 580,
+      btw: btw.hoog,
+    },
+    {
+      id: "1502",
+      name: "johny walker red",
+      price: 580,
+      btw: btw.hoog,
+    },
+    {
+      id: "1503",
+      name: "four roses",
+      price: 580,
+      btw: btw.hoog,
+    },
+    {
+      id: "1504",
+      name: "jack daniels",
+      price: 580,
+      btw: btw.hoog,
+    },
+    {
+      id: "1505",
+      name: "ballantine's",
+      price: 580,
+      btw: btw.hoog,
+    },
+    {
+      id: "1506",
+      name: "dimple",
+      price: 580,
+      btw: btw.hoog,
+    },
+  ],
+};
