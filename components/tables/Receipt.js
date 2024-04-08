5a6e5fcfda16afd526bb0bc007e691d57ade1887
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-04-08 21:12:32 +0200

    now shows loading icon when printer is printing

--- components/tables/Receipt.js
+++ components/tables/Receipt.js
@@ -1,15 +1,15 @@
 import receiptline from "receiptline";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // Function imports
 import euro from "@/functions/euro";
 import calculateTableTotal from "@/functions/calculateTableTotal";
 // Component imports
 import IconBtn from "@/components/IconBtn";
-import Snackbar from "@/components/Snackbar";
 // Icon imports
 import MinusIcon from "@/icons/MinusIcon";
 import PlusIcon from "@/icons/PlusIcon";
+import LoadingIcon from "@/icons/LoadingIcon";
 import PrintIcon from "@/icons/PrintIcon";
 // Firebase imports
 import { db } from "@/firebase/firebase";
@@ -18,6 +18,7 @@ import {
   updateDoc,
   setDoc,
   collection,
+  onSnapshot,
   getDocs,
 } from "firebase/firestore";
 
@@ -29,9 +30,22 @@ const Receipt = ({
   decrementDish,
 }) => {
   const [tip, setTip] = useState(table.tip);
-  const [snackbar, setSnackbar] = useState(false);
+  const [printJobs, setPrintJobs] = useState([]);
   const [formattedTip, setFormattedTip] = useState(`Tip: ${euro(table.tip)}`);
   const total = calculateTableTotal(table);
+
+  // Gets all the id's of printer jobs
+  useEffect(() => {
+    const printerRef = collection(db, "printer");
+    const unsubscribe = onSnapshot(printerRef, (snapshot) => {
+      const data = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setPrintJobs(data);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   // I need an array of items in food that needs to be printed
   const needsToBePrinted = table.food.filter((item) => !item.printed);
@@ -129,118 +143,110 @@ const Receipt = ({
   }
 
   return (
-    <>
-      <Snackbar snackbar={snackbar} setSnackbar={setSnackbar} />
-      <div
-        className={`px-8 pb-8 pt-4 h-full flex flex-col capitalize overflow-scroll ${
-          table.food.length ? "gap-4" : ""
-        }`}
-      >
-        <div className="text-base">
-          {table.food.map((dish) => {
-            return (
-              <div className="grid grid-cols-12" key={dish.id}>
-                <div className="col-span-2 max-w-[4.5rem] flex items-center justify-between pr-2">
-                  <IconBtn onClick={() => decrementDish(dish)}>
-                    <MinusIcon size="18" className="fill-main" />
-                  </IconBtn>
-                  <div className="">{dish.qwt}</div>
-                  <IconBtn onClick={() => incrementDish(dish)}>
-                    <PlusIcon size="18" className="fill-main" />
-                  </IconBtn>
-                </div>
-                <div
-                  onClick={() => {
-                    const ref = doc(db, `tables/${table.id}`);
-                    updateDoc(ref, {
-                      food: table.food.map((item) => {
-                        return item.id === dish.id
-                          ? { ...item, printed: !dish.printed }
-                          : item;
-                      }),
-                    });
-                  }}
-                  className={`col-span-7 font-medium cursor-pointer ${
-                    !dish.printed ? "text-main" : ""
-                  }`}
-                >
-                  {dish.name.nl}
-                </div>
-                <div className="col-span-3 text-right">{euro(dish.price)}</div>
-                <div className="col-span-2" />
-                <div className="col-span-7 -mt-1 text-xs">
-                  {dish.description}
-                </div>
+    <div
+      className={`px-8 pb-8 pt-4 h-full flex flex-col capitalize overflow-scroll ${
+        table.food.length ? "gap-4" : ""
+      }`}
+    >
+      <div className="text-base">
+        {table.food.map((dish) => {
+          return (
+            <div className="grid grid-cols-12" key={dish.id}>
+              <div className="col-span-2 max-w-[4.5rem] flex items-center justify-between pr-2">
+                <IconBtn onClick={() => decrementDish(dish)}>
+                  <MinusIcon size="18" className="fill-main" />
+                </IconBtn>
+                <div className="">{dish.qwt}</div>
+                <IconBtn onClick={() => incrementDish(dish)}>
+                  <PlusIcon size="18" className="fill-main" />
+                </IconBtn>
               </div>
-            );
-          })}
-        </div>
-        <div className="text-base">
-          {table.beverages.map((beverage) => {
-            return (
-              <div className="grid grid-cols-12" key={beverage.id}>
-                <div className="col-span-2 max-w-[4.5rem] flex items-center justify-between pr-2">
-                  <IconBtn onClick={() => decrementBeverage(beverage)}>
-                    <MinusIcon size="18" className="fill-main" />
-                  </IconBtn>
-                  <div className="">{beverage.qwt}</div>
-                  <IconBtn onClick={() => incrementBeverage(beverage)}>
-                    <PlusIcon size="18" className="fill-main" />
-                  </IconBtn>
-                </div>
-                <div className="col-span-7 font-medium">{beverage.name}</div>
-                <div className="col-span-3 text-right">
-                  {euro(beverage.price)}
-                </div>
+              <div
+                onClick={() => {
+                  const ref = doc(db, `tables/${table.id}`);
+                  updateDoc(ref, {
+                    food: table.food.map((item) => {
+                      return item.id === dish.id
+                        ? { ...item, printed: !dish.printed }
+                        : item;
+                    }),
+                  });
+                }}
+                className={`col-span-7 font-medium cursor-pointer ${
+                  !dish.printed ? "text-main" : ""
+                }`}
+              >
+                {dish.name.nl}
               </div>
-            );
-          })}
-        </div>
-        <div className="text-right border-t py-4 mt-4 text-base">
-          <div className="">subtotaal: {euro(total - table.tip)}</div>
-          <div className="">
-            <input
-              value={formattedTip}
-              onChange={(e) => {
-                const value = e.target.value.replace(/\D/g, ""); // Remove non-digits
-                const number = value === "" ? 0 : parseInt(value, 10);
-                setFormattedTip(`Tip: ${euro(number)}`);
-                setTip(number);
-              }}
-              className="text-right appearance-none focus:outline-none"
-              onBlur={() => {
-                const ref = doc(db, `tables/${table.id}`);
-                updateDoc(ref, {
-                  tip: tip,
-                });
-              }}
-            />
-          </div>
-        </div>
-        <div className="text-right border-t pt-4 text-base">
-          <div className="font-medium">totaal: {euro(total)}</div>
-        </div>
-        {needsToBePrinted.length > 0 && (
-          <button
-            onClick={async () => {
-              // We first check if the printer is busy
-              const ref = collection(db, "printer");
-              const snapshot = await getDocs(ref);
-              const printJobs = snapshot.docs.map((doc) => doc.data());
-              if (printJobs.length > 0) {
-                return setSnackbar("Printer is bezig probeer later nogmaals.");
-              }
-              printFood();
-              setSnackbar("Eten wordt afgedrukt.");
-            }}
-            className="button border mt-4 capitalize gap-2"
-          >
-            <PrintIcon />
-            eten afdrukken
-          </button>
-        )}
+              <div className="col-span-3 text-right">{euro(dish.price)}</div>
+              <div className="col-span-2" />
+              <div className="col-span-7 -mt-1 text-xs">{dish.description}</div>
+            </div>
+          );
+        })}
       </div>
-    </>
+      <div className="text-base">
+        {table.beverages.map((beverage) => {
+          return (
+            <div className="grid grid-cols-12" key={beverage.id}>
+              <div className="col-span-2 max-w-[4.5rem] flex items-center justify-between pr-2">
+                <IconBtn onClick={() => decrementBeverage(beverage)}>
+                  <MinusIcon size="18" className="fill-main" />
+                </IconBtn>
+                <div className="">{beverage.qwt}</div>
+                <IconBtn onClick={() => incrementBeverage(beverage)}>
+                  <PlusIcon size="18" className="fill-main" />
+                </IconBtn>
+              </div>
+              <div className="col-span-7 font-medium">{beverage.name}</div>
+              <div className="col-span-3 text-right">
+                {euro(beverage.price)}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="text-right border-t py-4 mt-4 text-base">
+        <div className="">subtotaal: {euro(total - table.tip)}</div>
+        <div className="">
+          <input
+            value={formattedTip}
+            onChange={(e) => {
+              const value = e.target.value.replace(/\D/g, ""); // Remove non-digits
+              const number = value === "" ? 0 : parseInt(value, 10);
+              setFormattedTip(`Tip: ${euro(number)}`);
+              setTip(number);
+            }}
+            className="text-right appearance-none focus:outline-none"
+            onBlur={() => {
+              const ref = doc(db, `tables/${table.id}`);
+              updateDoc(ref, {
+                tip: tip,
+              });
+            }}
+          />
+        </div>
+      </div>
+      <div className="text-right border-t pt-4 text-base">
+        <div className="font-medium">totaal: {euro(total)}</div>
+      </div>
+      {needsToBePrinted.length > 0 && (
+        <button
+          disabled={printJobs.length}
+          onClick={async () => {
+            printFood();
+          }}
+          className="button border mt-4 capitalize gap-2 flex items-center"
+        >
+          eten afdrukken
+          {printJobs.length ? (
+            <LoadingIcon className="animate-spin fill-main" />
+          ) : (
+            <PrintIcon />
+          )}
+        </button>
+      )}
+    </div>
   );
 };
 
