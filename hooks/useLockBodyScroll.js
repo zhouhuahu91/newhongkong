810c4f388daff794aa1ba9e647a38a2080934b73
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-07-18 20:18:08 +0200

    fixed bug where padding is added when there is no scroll bar in the first place

--- hooks/useLockBodyScroll.js
+++ hooks/useLockBodyScroll.js
@@ -22,14 +22,26 @@ function getScrollbarWidth() {
   return scrollbarWidth;
 }
 
+const hasScrollbar = () => {
+  return window.innerWidth > document.documentElement.clientWidth;
+};
+
 // Locks the body when a component is mounted and unlocks it when it is unmounted.
 const useLockBodyScroll = (open) => {
+  // We check outside the useEffect if the page has a scrollbar
+  const scrollbar = hasScrollbar();
+
   useEffect(() => {
-    const width = getScrollbarWidth();
     document.body.style.overflow = open ? "hidden" : "visible";
-    document.body.style.paddingRight = open ? `${width}px` : "0px";
+    // When the page has a scroll bar and the modal opens we need to add the width of the scroll bar to the right side of the page
+    // Because the modal removes the scroll bar.
+    if (scrollbar) {
+      const width = getScrollbarWidth();
+      document.body.style.paddingRight = open ? `${width}px` : "0px";
+    }
     return () => {
       document.body.style.overflow = "visible";
+      // When we exit we make sure we reset the padding to 0
       document.body.style.paddingRight = "0px";
     };
   }, [open]);
