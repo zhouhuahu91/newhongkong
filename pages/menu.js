e1111ef74c0398affe094be9ea48743b00d79455
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-08-11 22:46:23 +0200

    removed carts

--- pages/menu.js
+++ pages/menu.js
@@ -130,18 +130,18 @@ const Menu = () => {
           <div className="col-span-6 lg:col-span-5">
             {/* This span is needed so that the desktop starts on the same height as the menu without title. */}
             {/* <DesktopCart setOpen={setOpen} setDelivery={setDelivery} /> */}
-
+            {/* 
             {isAdmin === true ? (
               <AdminCart />
             ) : (
               <DesktopCart setOpen={setOpen} setDelivery={setDelivery} />
-            )}
+            )} */}
           </div>
         </div>
-        <MobileCart
+        {/* <MobileCart
           setDeliveryOrPickUpOpen={setOpen}
           setDelivery={setDelivery}
-        />
+        /> */}
       </div>
     </>
   );
