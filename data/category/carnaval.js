3b65023e3de36070f0682c3a83fe5816823d9883
Zhouhua Hu <zhouhua.hu@gmail.com>
2025-02-27 19:56:16 +0100

    added carnaval menu to restaurant

---
+++ data/category/carnaval.js
@@ -0,0 +1,20 @@
+import sidesOptions from "../options/sidesOptions";
+import btw from "../options/btw";
+
+export const carnaval = {
+  name: {
+    nl: "Carnaval",
+    en: "",
+    de: "",
+    zh: "狂欢全",
+  },
+  description: "",
+  id: "300",
+  price: 2690,
+  printed: false,
+  optionIsMain: false,
+  btw: btw.laag,
+  qwt: 1,
+  sides: sidesOptions,
+  printed: false,
+};
