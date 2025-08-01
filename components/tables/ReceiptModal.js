ff537de156b107df5118bf4b3b02f9b27aaa0656
Zhouhua Hu <zhouhua.hu@gmail.com>
2025-08-01 20:50:04 +0200

    you can now close receipt from mobile

--- components/tables/ReceiptModal.js
+++ components/tables/ReceiptModal.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 // we need a receipt modal for smallscreens
 import Receipt from "@/tables/Receipt";
 import Modal from "@/components/Modal";
+import IconBtn from "@/components/IconBtn";
+import CloseIcon from "@/icons/CloseIcon";
+import ReceiptIcon from "@/icons/ReceiptIcon";
 
 const ReceiptModal = ({
   table,
@@ -20,9 +23,20 @@ const ReceiptModal = ({
         type="button"
         className={`${buttonStyle} col-span-2 flex items-center justify-center gap-2`}
       >
+        <ReceiptIcon className="fill-inherit" />{" "}
         <span className="font-medium text-inherit">Rekening</span>
       </button>
-      <Modal toggle={open} close={() => setOpen(false)}>
+      <Modal
+        toggle={open}
+        close={() => setOpen(false)}
+        className="w-full h-full bg-white"
+      >
+        <div className="p-4 font-bold text-lg flex justify-between items-center">
+          <h1>Rekening</h1>
+          <IconBtn onClick={() => setOpen(false)}>
+            <CloseIcon />
+          </IconBtn>
+        </div>
         <Receipt
           table={table}
           incrementBeverage={incrementBeverage}
